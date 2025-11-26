522ccb57d2ae49715560f5f0c1f293798224f79e
md anas <anas2542wy@gmail.com>
2025-11-26 12:55:49 +0600

    final modify of signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -24,23 +24,23 @@ const Signup = () => {
 
   return (
     <div className="signup-container ">
-      <h2 className="text-2xl font-bold text-center text-blue-700">Signup</h2>
-      <form onSubmit={handleSignup}>
-        <input
+      <h2 className="text-3xl mb-2 mt-3  font-bold text-center text-blue-700">Signup</h2>
+      <form onSubmit={handleSignup} className="flex flex-col items-center text-xl ">
+        <input className="border-2 text-center mt-2 mb-3 rounded-full"
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-        <input
+        <input className="border-2 text-center mt-2 mb-3 rounded-full"
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <button type="submit">Sign Up</button>
+        <button className="bg-blue-700 text-white px-10 py-2 hover:bg-blue-900 rounded-full mb-4" type="submit">Sign Up</button>
       </form>
       {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
